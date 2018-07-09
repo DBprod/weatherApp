7b8b10e53c3d098547df8e6c7f5b167ddfba198b
danielbucci <danielbucci2@gmail.com>
2018-07-09 12:04:14 -0400

    mods

--- Front-End/app.js
+++ Front-End/app.js
@@ -1,5 +1,19 @@
+
+
 const locationinput = document.getElementById('locationInput');
-const displayWeatherLocation = document.getElementById('displayWeatherLocation');
+const displayWeatherLocation = document.getElementById('displayWeather');
+
+
+var xmlhttp = new XMLHttpRequest();
+var url = "myTutorials.txt";
+
+xmlhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+         console.log(JSON.parse(this.responseText));
+        
+    }
+};
+
 
 
 
